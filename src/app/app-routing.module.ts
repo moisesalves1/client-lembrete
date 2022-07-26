22f05027249d582d9a593c4147fee601a19c6485
Moises Alves <87319078+mooisesalves1@users.noreply.github.com>
2022-07-26 14:55:48 -0300

    Criado componente da página de edição

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaLembreteComponent } from './paginas/lista-lembrete/lista-lembrete.component';
 import { CriarLembreteComponent } from './paginas/criar-lembrete/criar-lembrete.component';
+import { EditarLembreteComponent } from './paginas/editar-lembrete/editar-lembrete.component';
 
 const routes: Routes = [
   { path: '', component: ListaLembreteComponent },
-  { path: 'lembrete/criar', component: CriarLembreteComponent }
+  { path: 'lembrete/criar', component: CriarLembreteComponent },
+  { path: 'lembrete/editar/:id', component: EditarLembreteComponent },
+  { path: '**', redirectTo: ''}
 ];
 
 @NgModule({
