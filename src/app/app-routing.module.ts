bb0fa678c5f4e6fda95a62ede33edbc06b4cc4d5
Moises Alves <87319078+mooisesalves1@users.noreply.github.com>
2022-07-26 13:57:44 -0300

    Criado componente da pagina de criação

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaLembreteComponent } from './paginas/lista-lembrete/lista-lembrete.component';
-import { FormLembreteComponent } from './compartilhado/form-lembrete/form-lembrete.component';
+import { CriarLembreteComponent } from './paginas/criar-lembrete/criar-lembrete.component';
 
 const routes: Routes = [
   { path: '', component: ListaLembreteComponent },
-  { path: 'lembrete/criar', component: FormLembreteComponent }
+  { path: 'lembrete/criar', component: CriarLembreteComponent }
 ];
 
 @NgModule({
